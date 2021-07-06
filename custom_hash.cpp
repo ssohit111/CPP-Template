6e51e8989d5b2354664825431d4a8a90b7c0d278
ssohit111 <55195332+ssohit111@users.noreply.github.com>
2021-07-06 15:35:39 +0530

    Update custom_hash.cpp

--- custom_hash.cpp
+++ custom_hash.cpp
@@ -19,7 +19,9 @@
 #define nl "\n" 
 #define all(v) v.begin(),v.end() 
 #define ms(a,x) memset(a,x,sizeof(a))
-#define fr(aa,bb) for(ll i=aa;i<=bb;i++)
+#define fr for(ll i=0;i<n;i++)
+#define frr for(ll i=1;i<=n;i++)
+// #define fr(aa,bb) for(ll i=aa;i<=bb;i++)
 #define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
 using namespace std;
 #pragma GCC target ("avx2")
@@ -66,6 +68,7 @@ struct custom_hash {
 };
 //template<class T, class H>using umap=unordered_map<T,H,custom_hash>;
 //template<class T>using uset=unordered_set<T,custom_hash>;
+// ll fact[1000005];
 ll dmod(ll x){
     return ((x+mod)%mod);
 }
@@ -81,15 +84,27 @@ ll modular_power(ll x,ll y){
 ll inv(ll x){
     return modular_power(dmod(x),mod-2);
 }
-
+ll nCr(ll n,ll r){
+    if(r>n) return 0;
+    else if(r<0) return 0;
+    else if(r==0||r==n)return 1;
+    ll ans=fact[n];
+    ans=(ans*modular_power(fact[n-r],mod-2))%mod; ans=(ans*modular_power(fact[r],mod-2))%mod;
+    return ans;
+}
 int main()
 {
     //clock_t clk = clock();
     fio
     //cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
-    ll t=1;
-    //cin>>t;
-    while(t--)
+    // fact[0]=1;fact[1]=1;fact[2]=2;
+    // for(ll i=3;i<=1000003;i++)
+    // {
+    //     fact[i]=(fact[i-1]*i)%mod;
+    // }
+    ll tt=1;
+    //cin>>tt;
+    while(tt--)
     {
         
     }
