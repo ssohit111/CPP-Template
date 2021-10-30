a81d563bcb67cdd1791b16696c9b127e75403b17
ssohit111 <55195332+ssohit111@users.noreply.github.com>
2021-10-30 17:13:31 +0530

    Update custom_hash.cpp

--- custom_hash.cpp
+++ custom_hash.cpp
@@ -1,33 +1,27 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
 #define fio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define ll long long 
+#define ll long long
 #define ull unsigned ll
-#define ld long double 
+#define ld long double
 #define mod 1000000007
-#define pb push_back 
+#define pb push_back
 #define pf push_front
-#define dll deque<ll> 
+#define dll deque<ll>
 #define vll vector<ll>
-#define vvll vector<vll> 
-#define pll pair<ll,ll> 
+#define vvll vector<vll>
+#define pll pair<ll,ll>
 #define vpll vector<pll>
 #define dpll deque<pll>
 #define mapll map<ll,ll>
 #define umapll unordered_map<ll,ll,custom_hash>
-#define nl "\n" 
-#define all(v) v.begin(),v.end() 
+#define nl "\n"
+#define all(v) v.begin(),v.end()
 #define ms(a,x) memset(a,x,sizeof(a))
-#define fr for(ll i=0;i<n;i++)
-#define frr for(ll i=1;i<=n;i++)
-// #define fr(aa,bb) for(ll i=aa;i<=bb;i++)
-#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+#define fr(a,b,c) for(ll a=b;a<=c;a++)
 using namespace std;
 #pragma GCC target ("avx2")
 #pragma GCC optimization ("O3")
 #pragma GCC optimization ("unroll-loops")
-using namespace __gnu_pbds;
 vector<string> split(const string& s, char c) {
     vector<string> v; stringstream ss(s); string x;
     while (getline(ss, x, c)) v.push_back(x); return move(v);
@@ -35,7 +29,7 @@ vector<string> split(const string& s, char c) {
 template<typename T, typename... Args>
 inline string arrStr(T arr, int n) {
     stringstream s; s << "[";
-    for(int i = 0; i < n - 1; i++) s << arr[i] << ",";
+    for (int i = 0; i < n - 1; i++) s << arr[i] << ",";
     s << arr[n - 1] << "]";
     return s.str();
 }
@@ -69,44 +63,55 @@ struct custom_hash {
 //template<class T, class H>using umap=unordered_map<T,H,custom_hash>;
 //template<class T>using uset=unordered_set<T,custom_hash>;
 // ll fact[1000005];
-ll dmod(ll x){
-    return ((x+mod)%mod);
+ll dmod(ll x) {
+    return ((x + mod) % mod);
 }
-ll modular_power(ll x,ll y){
-    ll ans=1;
-    while(y){
-        if(y&1)ans=dmod(ans*x);
-        y/=2;
-        x=dmod(x*x);
+ll modular_power(ll x, ll y) {
+    ll ans = 1;
+    while (y) {
+        if (y & 1)ans = dmod(ans * x);
+        y /= 2;
+        x = dmod(x * x);
     }
     return ans;
 }
-ll inv(ll x){
-    return modular_power(dmod(x),mod-2);
+ll inv(ll x) {
+    return modular_power(dmod(x), mod - 2);
 }
-ll nCr(ll n,ll r){
-    if(r>n) return 0;
-    else if(r<0) return 0;
-    else if(r==0||r==n)return 1;
-    ll ans=fact[n];
-    ans=(ans*modular_power(fact[n-r],mod-2))%mod; ans=(ans*modular_power(fact[r],mod-2))%mod;
-    return ans;
+// ll nCr(ll n,ll r){
+//     if(r>n) return 0;
+//     else if(r<0) return 0;
+//     else if(r==0||r==n)return 1;
+//     ll ans=fact[n];
+//     ans=(ans*modular_power(fact[n-r],mod-2))%mod; ans=(ans*modular_power(fact[r],mod-2))%mod;
+//     return ans;
+// }
+
+void start() {
+    fio
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
 }
+
 int main()
 {
-    //clock_t clk = clock();
-    fio
-    //cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    start();
+    // clock_t clk = clock();
     // fact[0]=1;fact[1]=1;fact[2]=2;
     // for(ll i=3;i<=1000003;i++)
     // {
     //     fact[i]=(fact[i-1]*i)%mod;
     // }
-    ll tt=1;
-    //cin>>tt;
-    while(tt--)
+    ll tt = 1;
+    cin >> tt;
+    while (tt--)
     {
         
     }
+    // cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
     return 0;
 }
